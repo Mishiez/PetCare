7e553b52592ded58f6e7c1f0b747b44f5898eccb
mishiez <michellegathonimain@gmail.com>
2024-12-07 22:20:02 +0300

    Added the mpesa API intergration

--- petcareapp/admin.py
+++ petcareapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from petcareapp.models import User,Product,Contact,Member
+from petcareapp.models import User, Product, Contact, Member, ImageModel
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Product)
 admin.site.register(Contact)
 admin.site.register(Member)
+admin.site.register(ImageModel)
--- petcareapp/forms.py
+++ petcareapp/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from petcareapp.models import Contact
+from petcareapp.models import Contact, ImageModel
+
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
+        fields = '__all__'
+
+class ImageUploadForm(forms.ModelForm):
+    class Meta:
+        model = ImageModel
         fields = '__all__'
